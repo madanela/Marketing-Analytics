02eefb76767733674963009ae36fd332dd867068
alenam0161 <alen.am0161@gmail.com>
2022-05-07 13:49:45 +0400

    basic structure of Frontend

--- multi/basic_page/layout.py
+++ multi/basic_page/layout.py
@@ -3,6 +3,15 @@ from dash import dcc, html
 
 import utils
 
+from dash import html, dcc, Dash, dash_table
+from dash.dependencies import Input, Output, State
+from datetime import datetime as dt, timedelta
+import plotly.express as px
+import pandas_datareader.data as web
+# from components.data import options
+import pandas as pd
+
+
 
 def layout(params):
     # This function must return a layout for the page
@@ -12,7 +21,7 @@ def layout(params):
     return dbc.Col(
         [
             dbc.Row( # Generation parameters
-                [
+                [   
                     dbc.Card(
                         [
                             dbc.CardHeader("Select Generation Parameters"),
@@ -20,19 +29,62 @@ def layout(params):
                             dbc.CardBody(
                                 dbc.Row(
                                     [ 
-                                        dbc.Button(
-                                            "Gerenate",
-                                            id=__package__ + "-button",
-                                            color="primary",
-                                        ),
-                                        dbc.InputGroup(
-                                            [
-                                                dbc.InputGroupText("Number of Groups"),
-                                                dbc.Input(placeholder="Enter", type="number"),
-                                            ],
-                                            className="mb-3",
-                                        )
-                                    ],
+                                        # Block 1, Button Generate
+                                        dbc.Col([
+                                            dbc.Button(
+                                                "Gerenate",
+                                                id=__package__ + "-button",
+                                                color="primary",
+                                            )
+                                        ]),
+                                        # Block 2 
+                                        dbc.Col([
+                                            dbc.InputGroup(
+                                                [
+                                                    dbc.InputGroupText("Number of Treatment Groups"),
+                                                    dbc.Input(placeholder="Enter",
+                                                              type="number",
+                                                              id = 'group_number'),
+                                                ],
+                                                className="mb-3",
+                                            )
+                                        ]),
+                                        # Block 3
+                                        dbc.Col([
+                                            dbc.InputGroup(
+                                                [
+                                                    dbc.InputGroupText("Number of Datapoints"),
+                                                    dbc.Input(placeholder="Enter", 
+                                                              type="number",
+                                                              id = 'datapoint_number'),
+                                                ],
+                                                className="mb-3",
+                                            )
+                                        ]),
+                                        # Block 4
+                                         dbc.Col([
+                                            dbc.InputGroup(
+                                                [
+                                                    dbc.InputGroupText("Number of Features"),
+                                                    dbc.Input(placeholder="Enter", 
+                                                              type="number",
+                                                              id = 'feature_number'),
+                                                ],
+                                                className="mb-3",
+                                            )
+                                        ]),
+                                          dbc.Col([
+                                            dbc.InputGroup(
+                                                [
+                                                    dbc.InputGroupText("Number of Features"),
+                                                    dbc.Input(placeholder="Enter", 
+                                                              type="number",
+                                                              id = 'feature_number'),
+                                                ],
+                                                className="mb-3",
+                                            )
+                                        ]),
+                                    ]
                                 )
                             ),
                         ],
@@ -41,25 +93,115 @@ def layout(params):
                 ],
                 class_name="pd-2",
             ),
-            dbc.Row( # Visualisation  parameters
-                [
+             dbc.Row( # Select which option to use parameters
+                [   
                     dbc.Card(
                         [
-                            dbc.CardHeader("Some points on a chart"),
+                            dbc.CardHeader("Select The Tool"),
+                            
                             dbc.CardBody(
-                                [
-                                    dcc.Graph(
-                                        figure=utils.empty_figure(),
-                                        id=__package__ + "-chart",
-                                        config={"displayModeBar": False},
-                                    )
-                                ],
+                                dbc.Row([
+                                    # Visualisation
+                                    dbc.Col([
+                                        dbc.Label("Choose Which option to use"),
+                                        dbc.RadioItems(
+                                            options=[
+                                                {"label": "Option 1", "value": 1},
+                                                {"label": "Option 2", "value": 2},
+                                            ],
+                                            value=1,
+                                            id="radioitems-inline-input",
+                                            inline=True,
+                                        ),
+                                    ]),
+                                    # Statistics
+                                    dbc.Col([
+                                        dbc.Label("Choose Which option to use"),
+                                        dbc.RadioItems(
+                                            options=[
+                                                {"label": "Option 1", "value": 1},
+                                                {"label": "Option 2", "value": 2},
+                                            ],
+                                            value=1,
+                                            id="radioitems-inline-input",
+                                            inline=True,
+                                        ),
+                                    ]),
+                                ])
                             ),
                         ],
                     ),
+                    html.Br(),
+                ],
+                class_name="pd-2",
+            ),
+            dbc.Row( # Visualisation  
+                [
+                    dbc.Col([
+                        dbc.Card(
+                            [
+                                dbc.CardHeader("Some points on a chart"),
+                                dbc.CardBody(
+                                    [
+                                        dcc.Graph(
+                                            figure=utils.empty_figure(),
+                                            id=__package__ + "-chart",
+                                            config={"displayModeBar": False},
+                                        )
+                                    ],
+                                ),
+                            ],
+                        ),
+                    ]),
+                    dbc.Col([
+                        dbc.Card(
+                            [
+                                dbc.CardHeader("Some points on a chart"),
+                                dbc.CardBody(
+                                    [
+                                        dcc.Graph(
+                                            figure=utils.empty_figure(),
+                                            id=__package__ + "-chart",
+                                            config={"displayModeBar": False},
+                                        )
+                                    ],
+                                ),
+                            ],
+                        ),
+                    ]),
                 ],
                 class_name="pd-2",
             ),
         ],
         class_name="mx-5 mt-5",
     )
+
+from app import app
+from random import random
+
+import dash
+# Any callbacks for the page should go here
+@app.callback(
+    dash.dependencies.Output(__package__ + "-chart", "figure"),
+    [dash.dependencies.Input(__package__ + "-button", "n_clicks")],
+)
+def button_click(n_update):
+    # Plot some random numbers on a chart
+    print("true")
+    res = {
+        "data": [
+            {
+                "x": [0 * random() - 1 for x in range(10)],
+                "y": [2 * random() - 1 for y in range(10)],
+                "mode": "markers",
+                "name": "Demo",
+            },
+        ],
+        "layout": {
+            "margin": {"l": 0, "r": 0, "t": 0, "b": 0, "pad": 0},
+            "xaxis": {"range": [-1, +1]},
+            "yaxis": {"range": [-1, +1]},
+        },
+    }
+    print(res)
+    return res
