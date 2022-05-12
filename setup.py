8715fffa2edc8f7409b45260f38abad9ca152bfe
alenam0161 <alen.am0161@gmail.com>
2022-05-13 01:47:10 +0400

    setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+   name='A/B testing platform',
+   version='1.0',
+   description='Easy usage of data and research',
+   author='AUA student',
+   author_email='alen_adamyan@edu.aua.am',
+   packages=['foo'],  #same as name
+   python_requires='=3.9.12',
+  # install_requires=['wheel', 'bar', 'greek'], #external packages as dependencies
+)
